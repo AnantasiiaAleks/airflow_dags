5fb79dbce79f548cbfed3dc20fd1c3db73797fd6
Anastasiia <chiffka@gmail.com>
2025-05-20 08:26:47 +0300

    DAG с прогнозом погоды и ветвлением жарко/холодно11

--- dags/cold_warm_DAG.py
+++ dags/cold_warm_DAG.py
@@ -17,7 +17,7 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.utils.dates import days_ago
 import requests
 
-API_KEY = '28bb22fe4d94d7d3579d1d602e34d830'
+API_KEY = '28bb22fe4d94d7d3579d1d602e34d8302011'
 LOCATION = 'Moscow'
 
 def get_temperature(**context):
